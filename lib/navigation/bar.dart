975dc664abe7c54a4dd14d8ea421d766fa035b7d
Mia <miathewiccancatgirl@gmail.com>
2026-03-18 09:46:48 -0500

    update

--- lib/navigation/bar.dart
+++ lib/navigation/bar.dart
@@ -22,6 +22,7 @@ class _BarState extends State<Bar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       body: IndexedStack(index: _selectedIndex, children: const [Dashboard(), Addons(), Settings()]),
       bottomNavigationBar: BottomNavigationBar(
         items: [BottomNavigationBarItem(icon: Icon(Icons.home_outlined), activeIcon: Icon(Icons.home_filled), label: 'Home'),
