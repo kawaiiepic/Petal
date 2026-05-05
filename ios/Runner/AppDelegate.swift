aa231d40973638d451b75b9414e0fd19744e5422
Mia <miathewiccancatgirl@gmail.com>
2026-05-04 23:21:56 -0500

    fix: test pip functionality

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,14 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+
+    let factory = PipPlayerViewFactory()
+    registrar(forPlugin: "pip_player_view")?.register(
+      factory,
+      withId: "pip_player_view"
+    )
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
