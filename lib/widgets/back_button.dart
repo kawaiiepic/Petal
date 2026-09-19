6ddc72be4adae97aa865d5e8699ff15e2ab6af79
Mia <59005594+kawaiiepic@users.noreply.github.com>
2026-09-19 03:31:26 -0400

    offset BackButton by MediaQuery padding so it clears the status bar

--- lib/widgets/back_button.dart
+++ lib/widgets/back_button.dart
@@ -4,6 +4,15 @@ import 'package:shadcn_flutter/shadcn_flutter_experimental.dart';
 
 class BackButton extends StatelessWidget {
   @override
-  Widget build(BuildContext context) =>
-      OutlineButton(density: ButtonDensity.icon, onPressed: () => PetalApp.rootNavigatorKey.currentContext?.pop(), child: const Icon(LucideIcons.chevronLeft));
+  Widget build(BuildContext context) {
+    final topInset = MediaQuery.paddingOf(context).top;
+    return Padding(
+      padding: EdgeInsets.only(top: topInset),
+      child: OutlineButton(
+        density: ButtonDensity.icon,
+        onPressed: () => PetalApp.rootNavigatorKey.currentContext?.pop(),
+        child: const Icon(LucideIcons.chevronLeft),
+      ),
+    );
+  }
 }
