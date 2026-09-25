c178eed47749fa5ebe925edcf766b1c7a2faff65
Mia <59005594+kawaiiepic@users.noreply.github.com>
2026-09-25 10:26:28 -0400

    Add a collapsible home section header

---
+++ lib/widgets/home_section.dart
@@ -0,0 +1,40 @@
+import 'package:shadcn_flutter/shadcn_flutter_experimental.dart';
+import 'package:sizer/sizer.dart';
+
+class HomeSection extends StatefulWidget {
+  final String title;
+  final Widget child;
+  final bool initiallyOpen;
+
+  const HomeSection({super.key, required this.title, required this.child, this.initiallyOpen = true});
+
+  @override
+  State<HomeSection> createState() => _HomeSectionState();
+}
+
+class _HomeSectionState extends State<HomeSection> {
+  late bool _open = widget.initiallyOpen;
+
+  @override
+  Widget build(BuildContext context) {
+    final style = TextStyle(fontSize: Device.screenType == ScreenType.desktop ? 12.sp : 16.sp, fontWeight: FontWeight.w600);
+    return Column(
+      children: [
+        Button(
+          style: ButtonVariance.ghost,
+          onPressed: () => setState(() => _open = !_open),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Row(
+              children: [
+                Expanded(child: Text(widget.title, style: style, textAlign: TextAlign.center)),
+                Icon(_open ? LucideIcons.chevronUp : LucideIcons.chevronDown, size: 16),
+              ],
+            ),
+          ),
+        ),
+        if (_open) widget.child,
+      ],
+    );
+  }
+}
