3f5085d2b79182751927d475b9ab85648ba6dcf2
Mia <59005594+kawaiiepic@users.noreply.github.com>
2026-09-24 20:07:43 -0400

    Move library import off the account menu and onto Settings

--- lib/navigation/profile.dart
+++ lib/navigation/profile.dart
@@ -67,7 +67,6 @@ class _Profile extends State<UserProfile> {
                         MenuDivider(),
                         MenuButton(onPressed: (_) async => PetalApp.rootNavigatorKey.currentContext?.push('/addons'), child: const Text('Addons')),
                         MenuButton(onPressed: (_) async => PetalApp.rootNavigatorKey.currentContext?.push('/settings'), child: const Text('Settings')),
-                        MenuButton(onPressed: (_) async => PetalApp.rootNavigatorKey.currentContext?.push('/trakt-import'), child: const Text('Import from Trakt')),
                         MenuDivider(),
                         MenuButton(
                           onPressed: (_) async {
